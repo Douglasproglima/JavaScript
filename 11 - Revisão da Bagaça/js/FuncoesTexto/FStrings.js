d3ef3275a0102b9ecb8ccee056ea33a74cea9fe9
Douglas Lima <douglasproglima@gmail.com>
2016-06-01 22:35:51 -0300

    Função formatarCamposDinamicamente() e funções de validações

--- 11 - Revisão da Bagaça/js/FuncoesTexto/FStrings.js
+++ 11 - Revisão da Bagaça/js/FuncoesTexto/FStrings.js
@@ -1,3 +1,16 @@
+//Função dinâmica para formatação de CEP, CPF, Telefone etc. é só passar a mascará
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
+
+
 function verificarTextoValido(texto){
 	if (texto.length < 1)
 		return false;
@@ -59,4 +72,129 @@ function verificaNumero(evt){
 	}else{
 		return true;
 	}
+}
+
+function validarEmail(input){
+	var email = input.value;
+	if(!verificarTextoValido(email)){
+		return false;
+	}else{
+		//A função indexOf() irá retornar -1 se o elemento não existir
+		//Se o elemento existir vai retorna a posição atual;
+		var indice = email.indexOf("@");
+		
+		//Verifica se existe o @
+		if(indice > 2){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+function validarData(input){
+	var data = input.value.split("/");
+	if (data.length < 3) {
+		return false;
+	}
+	
+	for(var i = 0; i <  data.length; i++){
+		//Valida os dias
+		if(i == 0 && (isNaN(data[i]) || data[i] < 1 || data[i] > 31) ){
+			return false;
+		}
+		
+		//Valida os meses
+		if(i == 1 && (isNaN(data[i]) || data[i] < 1 || data[i] > 12) ){
+			return false;
+		}
+		
+		if(i == 2 && (isNaN(data[i]) || data[i] < 1900 || data[i] > 2050) ){
+			return false;
+		}
+	}
+	return true;
+}
+
+function formatarData(input){
+	var data = input.value;
+	//Se o comprimeto da data for maior que 2 e a barra não existir
+	if(data.length > 2 && data.indexOf("/") == -1){
+		data = data.substring(0, 2) + "/" + data.substring(2, data.length);
+	}
+	
+	//Se o comprimento for maior do que 5 e se o index da barra a partir da posição 5 se não existir adiciona
+	if(data.length > 5 && data.indexOf("/", 5) == -1){
+		data = data.substring(0, 5) + "/" + data.substring(5, data.length);
+	}
+	
+	input.value = data;
+}
+
+//Função para centralizar no campo que tiver advergência
+function rolagem(elemento){
+	var posicaoY = 0;
+	
+	//Verificar se existe o offSetParant (Distância do elemento para o elemento pai)
+	if(elemento.offsetParent){
+		do{
+			posicaoY += elemento.offsetTop; //Pega a distância do topo
+			elemento = elemento.offsetParent;
+			
+		//Se estiver o elemento continua
+		}while(elemento)
+	}
+	
+	var tamanhoJanela = window.innerHeight; 		 //Pega o tamanho da tela do computador
+	var posicaoAtual = self.pageYOffset; 			 //`Posição atual da página
+	var posicaoFinal = posicaoY - (tamanhoJanela/4); //Posição final desejada
+	
+	//Enquanto a posição atual for maior do que a posição final será decrementado a posição atual em 5 pixel
+	while(posicaoAtual > posicaoFinal){
+		posicaoAtual -= 5;
+		window.scroll(0, posicaoAtual);
+	}
+	
+}
+
+//Formatar CPF
+function mascara(o,f){
+	v_obj=o
+	v_fun=f
+	setTimeout("execmascara()",1)
+}
+
+function execmascara(){
+	v_obj.value=v_fun(v_obj.value)
+}
+
+function cpf_mask(v){
+	v=v.replace(/\D/g,"")                 //Remove tudo o que não é dígito
+	v=v.replace(/(\d{3})(\d)/,"$1.$2")    //Coloca ponto entre o terceiro e o quarto dígitos
+	v=v.replace(/(\d{3})(\d)/,"$1.$2")    //Coloca ponto entre o setimo e o oitava dígitos
+	v=v.replace(/(\d{3})(\d)/,"$1-$2")   //Coloca ponto entre o decimoprimeiro e o decimosegundo dígitos
+	return v
+}
+
+
+//Conjunto de funções para formatar o telefone
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+
+function mtel(v){
+    v=v.replace(/\D/g,"");             		//Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+
+function id( el ){
+	return document.getElementById( el );
 }
